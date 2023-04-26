1f35d270dabf5fb259e82ebc1feee940ddd4361d
Brillant-Minds-Grupo-5 <mosanz02@gmail.com>
2023-04-26 17:53:51 -0500

    V.0.1

---
+++ Taller1.js
@@ -0,0 +1,12 @@
+
+
+var a =prompt("ingrese el primer numero");
+var b =prompt("ingrese el segundo numero");
+
+            if(a===b)
+                alert("son iguales");
+            if(a>b)
+                alert("el primer numero mayor es : "+a);
+            if(b>a)
+                alert("el segundo numero mayor es : "+b);
+            
